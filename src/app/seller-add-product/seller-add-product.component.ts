ddf46222dfc75eaf701a380b37aaad6bf500b1d9
chettipally suribabu <Suribabu.C@cloudtaru.com1>
2024-07-30 15:02:07 +0530

    from group , form controls added and payload is prepared to send data to the backend

--- src/app/seller-add-product/seller-add-product.component.ts
+++ src/app/seller-add-product/seller-add-product.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators,FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-seller-add-product',
@@ -6,5 +7,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./seller-add-product.component.css']
 })
 export class SellerAddProductComponent {
+  Addproductform !:FormGroup
+
+  constructor(private fb:FormBuilder){
+
+  }
+  ngOnInit(){
+    this.Addproductform = this.fb.group({
+      product_name:[''],
+      product_price :[''],
+      product_category:[''],
+      product_color:[''],
+      product_description:[''],
+    })
+  }
+
+  submit(){
+    console.log('---->>add product form',this.Addproductform.value)
+    console.log('----->>product_name',this.Addproductform.value.product_name)
+    const formData = new FormData();
+    formData.append('product_name', this.Addproductform.value.product_name);
+    formData.append('product_price', this.Addproductform.value.product_price);
+    formData.append('product_category', this.Addproductform.value.product_category);
+    formData.append('product_color', this.Addproductform.value.product_color);
+    formData.append('product_description', this.Addproductform.value.product_description);
+    console.log('--------->>>formData',formData)
+    formData.forEach((value, key) => {
+      console.log(key + ': ' + value);
+    });
+  }
 
 }
