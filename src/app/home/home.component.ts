d791a16bd4acdba7d39f9a49a9280e0d1fcfb7ae
chettipally suribabu <Suribabu.C@cloudtaru.com1>
2024-09-20 16:45:42 +0530

    carousal is added to home page and trendy products list is displaying

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { environment } from 'src/envronments/environment';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  products: any[] = [];
+  trendyproducts: any[]=[];
+  constructor(private productservice: ProductService){
+
+  }
+  ngOnInit(){
+    this.productservice.getAllproduct().subscribe(res => {
+      console.log('----->>>taking the products to home page', res)
+      res.data.forEach((product: { product_file_path: string; }) => {
+        product.product_file_path = environment.apiEndpoint + product.product_file_path;
+        console.log('--->>product.product_file_path,', product.product_file_path)
+      });
+      this.products = res.data.slice(0, 5)
+      this.trendyproducts = res.data
+      console.log('------->>>this.products ', this.products)
+    })
+  }
 
 }
